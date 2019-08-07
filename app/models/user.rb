2db3242be4e914080b3b44f66eab9d770a633c55
EC2 Default User <ec2-user@ip-172-31-85-244.ec2.internal>
2019-08-07 07:03:11 +0000

    seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   has_many :matching_tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_08_05_065732) do
     t.integer "salary"
     t.datetime "registration_deadline"
     t.datetime "task_deadline"
+    t.string "time_to_do"
     t.boolean "veteran"
     t.string "descript_url"
     t.string "money_unit"
