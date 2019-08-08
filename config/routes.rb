ccaf82968454061a7e5698d57f1c7d81c1a84b96
taku56 <al18013@shibaura-it.ac.jp>
2019-08-08 08:25:04 +0000

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "users/profile", :to => "users#show"
+  get "users/profile", :to => "users#company"
   get "users/student", :to => "users#sprof"
   resources :users
   resources :tasks
